412410040187dab6ce84cfcce06d4dda07d17a2e
ickiie <nicolasries8@gmail.com>
2022-02-11 10:07:52 -0700

    update

--- Assets/Common/Damage.cs
+++ Assets/Common/Damage.cs
@@ -5,17 +5,31 @@ using UnityEngine;
 public class Damage : MonoBehaviour
 {
     [SerializeField] float damage = 0;
+    [SerializeField] bool oneTime = true;
+    [SerializeField] float damageTimer = 1;
 
     private void OnTriggerEnter(Collider other)
     {
+        if (!oneTime) { return; }
+        
         if (other.gameObject.TryGetComponent<Health>(out Health health))
         {
             health.Damage(damage);
         }
-/*        if (gameObject.TryGetComponent<Health>(out health) && other.gameObject.TryGetComponent<Damage>(out Damage otherDamage))
+        /*if (gameObject.TryGetComponent<Health>(out health) && other.gameObject.TryGetComponent<Damage>(out Damage otherDamage))
         {
             health.Damage(otherDamage.damage);
         }*/
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (oneTime) { return; }
+
+        if (other.gameObject.TryGetComponent<Health>(out Health health))
+        {
+            health.Damage(damage * Time.deltaTime);
+        }
     }
 
 
--- Assets/RollerBall/Scripts/RollerCoin.cs
+++ Assets/RollerBall/Scripts/RollerCoin.cs
@@ -2,31 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RollerCoin : MonoBehaviour
+public class RollerCoin : MonoBehaviour, IDestructable
 {
-    public float amplitude;
-    public float rate;
-    public float spinRate;
-
-    Vector3 initialPosition;
-    float time;
-    float angle;
-
-    void Start()
+    [SerializeField] int points;
+    public void Destroyed()
     {
-        time = Random.Range(0f, 5f);
-        angle = Random.Range(0f, 360f);
-        initialPosition = transform.position;
+        RollerGameManager.Instance.Score += points;
     }
 
-
-    void Update()
-    {
-        time += Time.deltaTime * rate;
-        angle += Time.deltaTime * spinRate;
-
-        Vector3 offset = Vector3.up * Mathf.Sin(time) * amplitude;
-        transform.position = initialPosition + offset;
-        transform.rotation = Quaternion.AngleAxis(angle, Vector3.up);
-    }
 }
